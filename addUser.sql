e06389de3b3e70bcc05e7357444badee209908e9
LukaM <luka.mestrovic@fer.hr>
2025-12-15 18:59:30 +0100

    Popravak addUser.sql

--- addUser.sql
+++ addUser.sql
@@ -1,11 +1,6 @@
-
+-- Dodaj si usera sa ADMIN ulogom (iduloge = 1)
 INSERT INTO Korisnici (imeKorisnik, prezimeKorisnik, email, provider, providerUserId, idUloge, datumRegistracije)
 VALUES ('Luka', 'Mestrovic', 'luka.mestarm@gmail.com', 'google', 'luka.mestarm@gmail.com', 1, CURRENT_DATE)
 ON CONFLICT (email) 
-DO UPDATE SET 
-    imeKorisnik = EXCLUDED.imeKorisnik,
-    prezimeKorisnik = EXCLUDED.prezimeKorisnik,
-    provider = EXCLUDED.provider,
-    providerUserId = EXCLUDED.providerUserId,
-    idUloge = EXCLUDED.idUloge;
+DO NOTHING;
 
