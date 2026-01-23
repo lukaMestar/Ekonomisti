59410477ac4c09ea0ee4a03daacff2b2f6007778
Luka Salopek <luka.salopek47@gmail.com>
2026-01-23 11:42:27 +0100

    Ispravak putnih naloga

--- react/frontend/src/USERI/KLIJENT/KlijentContext.jsx
+++ react/frontend/src/USERI/KLIJENT/KlijentContext.jsx
@@ -2,23 +2,23 @@ import { createContext, useContext, useState, useEffect } from "react";
 import { API_URL } from "../../config.js";
 import { apiCall } from "../../api.js";
 
-
 const KlijentContext = createContext();
 
 export function KlijentProvider({ children }) {
-
   const [firmaNaziv, setFirmaNaziv] = useState(null);
   const [stanjeRac, setStanjeRac] = useState(null);
   const [emailIzvj, setEmailIzvj] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [firmaId, setFirmaId] = useState(null);
 
-  const[listaZaposlenika, setListaZaposlenika] = useState([]);
-  const[listaRacuna, setListaRacuna] = useState([]);
-  const[PN, setPutniNalozi] = useState([]);
-  const[PNN, setPutniNaloziNeodradjeni] = useState([]);
-  const[F, setF] = useState([]);
-  const[FN, setFN] = useState([]);
+  const [listaZaposlenika, setListaZaposlenika] = useState([]);
+  const [listaRacuna, setListaRacuna] = useState([]);
+  const [PN, setPutniNalozi] = useState([]);
+  const [PNN, setPutniNaloziNeodradjeni] = useState([]);
+  const [F, setF] = useState([]);
+  const [FN, setFN] = useState([]);
 
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchAll() {
@@ -36,46 +36,45 @@ export function KlijentProvider({ children }) {
           apiCall(`${API_URL}/api/izvjestaj/listaF`),
           apiCall(`${API_URL}/api/izvjestaj/listaPN`)
         ]);
-  
+
         if (firmaRes.ok) {
           const firmaData = await firmaRes.json();
           setFirmaNaziv(firmaData.nazivFirme);
           setStanjeRac(firmaData.stanjeRacuna);
           setEmailIzvj(firmaData.emailIzvjestaj);
+          setUserId(firmaData.id);
+          setFirmaId(firmaData.firmaId);
         }
-  
+
         if (racuniRes.ok) setListaRacuna(await racuniRes.json());
         if (zaposleniciRes.ok) setListaZaposlenika(await zaposleniciRes.json());
         if (fRes.ok) {
           const FakturData = await fRes.json();
           setF(FakturData.odradjeni);
           setFN(FakturData.neodradjeni);
-        };
+        }
         if (pnRes.ok) {
           const PNData = await pnRes.json();
           setPutniNalozi(PNData.odradjeni);
           setPutniNaloziNeodradjeni(PNData.neodradjeni);
-        };
-  
+        }
+
       } catch (err) {
         alert("Greška pri dohvaćanju podataka" + err);
       } finally {
         setLoading(false);
       }
     }
-  
+
     fetchAll();
   }, []);
 
-  
-  console.log("Tuu saammmmmm --------------------------");
-
-  if (loading) { return <p>Učitavanje podataka...</p>;}
+  if (loading) { return <p>Učitavanje podataka...</p>; }
 
   return (
     <KlijentContext.Provider
       value={{
-        firmaNaziv, stanjeRac, emailIzvj, listaZaposlenika, listaRacuna, PN, PNN, F, FN
+        userId, firmaId, firmaNaziv, stanjeRac, emailIzvj, listaZaposlenika, listaRacuna, PN, PNN, F, FN
       }}
     >
       {children}
